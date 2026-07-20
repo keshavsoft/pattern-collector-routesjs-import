6a1d8a63811a532c218624857ed268bcf6045548
keshavsoft <nknnkumar@yahoo.com>
2026-07-20 17:41:08 +0530

    KeshavSoft

---
+++ bin/v2/index.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+
+import getAllMatches from "pattern-collector";
+
+// const searchString = /^[ \t]*router\.use\b.*\);\s*$/gm;
+// const searchString = /^[ \t]*router\.use\b.*?;/gm;
+// const searchString = /^[ \t]*import\b.*;/gm;
+// const searchString = /^[ \t]*import\b.*from\s+['"][^'"]+['"];/gm;
+const searchString = /^[ \t]*import\b.*from\s+['"]\.[^'"]*['"];/gm;
+
+const fileNameToPull = "routes.js";
+
+const startFunc = ({ inFilePath }) => {
+    try {
+        const filePath = path.join(inFilePath, fileNameToPull);
+
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+
+        const matches = getAllMatches({
+            fileContent: fileContent, searchString
+        });
+
+        return matches;
+    } catch (error) {
+        console.error("Error running test:", error.message);
+    };
+
+    return [];
+};
+
+export default startFunc;
